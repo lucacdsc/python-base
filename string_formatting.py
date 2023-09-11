ed24094c663a40bea9b45126ce94496ea7305d85
Luca Clemente <clementluca6@gmail.com>
2023-09-11 18:35:27 -0300

    add string_formatting.py

---
+++ string_formatting.py
@@ -0,0 +1,8 @@
+#What ways can we format strings
+live,de,python = 'live','de','python'
+'%s %s %s' % (live,de,python)
+'{} {} {}'.format(live,de,python)
+f'{live} {de} {python}'
+
+#format
+'{0}{1}'.format('Luca','Clemente')
